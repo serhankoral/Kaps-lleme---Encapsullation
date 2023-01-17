1937b72d9b5d43b137a80024473a72812238f992
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-17 15:33:12 +0300

    Proje dosyası ekle.

---
+++ Kapsülleme - Encapsullation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Kapsülleme___Encapsullation
+{
+        /* 
+         * Kapsülleme ve Encapsullation
+         * Kapsülleme ve Encapsullation
+         * Girilen verinin sıkıştırılması ve açılmasıdır
+         * Veri girişinde kontrol edilir sonrasında bu göre set edilir
+         * Veri get edilirken kontrol edilir ona göre sonuç verilir
+         * hazir kod blogu için prob ve probfull kullanılır
+         * Yapılacaklar
+         * Firma ürün fiyatları için bir program oluşturalım
+         * Aynı zamanda oluşturulurken kalıtım kullanalım
+         * 
+        */
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            ÜrünBilgisi ürünBilgisi=new ÜrünBilgisi();
+            ürünBilgisi.UrunAdi = "Ram";
+            ürünBilgisi.UrunFiyat = 400;
+            ürünBilgisi.UrunAdet = 20;
+        }
+    }
+  
+}
---
+++ Kapsülleme - Encapsullation/ÜrünBilgisi.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kapsülleme___Encapsullation
+{
+    class ÜrünBilgisi
+    {
+        private string urunAdi;
+        public string UrunAdi
+        {
+            get { return urunAdi; }
+            set
+            {
+                if (check(value, "Ürün Adını Boş Geçemessiniz!"))
+                {
+                    urunAdi = value;
+                }
+            }
+        }
+
+        private int urunFiyat;
+        public int UrunFiyat
+        {
+            get { return urunFiyat; }
+            set
+            {
+                if (check(value, "Ürün Fiyatı Sıfırdan Büyük Olmalı!"))
+                {
+                    urunFiyat = value;
+                }
+            }
+        }
+        private int urunAdet;
+
+        public int UrunAdet
+        {
+            get { return urunAdet; }
+            set
+            {
+                if (check(value, "Ürün Adeti Sıfırdan Büyük Olmalı!"))
+                {
+                    urunFiyat = value;
+                }
+            }
+        }
+        private bool check(object checkObject, string msj)
+        {
+            if (checkObject.GetType() == typeof(int))
+            {
+                if ((int)checkObject <= 0)
+                {
+                    Console.WriteLine(msj);
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+            if (checkObject.GetType() == typeof(string))
+            {
+                if (string.IsNullOrEmpty((string)checkObject))
+                {
+                    Console.WriteLine(msj);
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+    }
+}
